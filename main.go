594f1d2d322cd3b30fdbeccc3b5c17a9aa7bbd21
Chonlasit <chonlasit.s@arise.tech>
2023-02-16 11:34:10 +0700

    update main

--- main.go
+++ main.go
@@ -1,34 +1,27 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-
-	"github.com/stretchr/testify/mock"
-)
-
 func main() {
-	c := CustomerRepositoryMock{}
+	// c := CustomerRepositoryMock{}
 
-	c.On("GetCustomer", 1).Return("bond", 19, nil)
-	c.On("GetCustomer", 2).Return("", 0, errors.New("no found"))
+	// c.On("GetCustomer", 1).Return("bond", 19, nil)
+	// c.On("GetCustomer", 2).Return("", 0, errors.New("no found"))
 
-	name, age, err := c.GetCustomer(2)
-	if err != nil {
-		return
-	}
+	// name, age, err := c.GetCustomer(2)
+	// if err != nil {
+	// 	return
+	// }
 
-	fmt.Println(name, age)
+	// fmt.Println(name, age)
 }
 
-type CustomerRepository interface {
-	GetCustomer(id int) (name string, age int, err error)
-}
-type CustomerRepositoryMock struct {
-	mock.Mock
-}
+// type CustomerRepository interface {
+// 	GetCustomer(id int) (name string, age int, err error)
+// }
+// type CustomerRepositoryMock struct {
+// 	mock.Mock
+// }
 
-func (m *CustomerRepositoryMock) GetCustomer(id int) (name string, age int, err error) {
-	args := m.Called(id)
-	return args.String(0), args.Int(1), args.Error(2)
-}
+// func (m *CustomerRepositoryMock) GetCustomer(id int) (name string, age int, err error) {
+// 	args := m.Called(id)
+// 	return args.String(0), args.Int(1), args.Error(2)
+// }
